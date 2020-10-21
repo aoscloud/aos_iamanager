1538b2d4e2354f501cfefeda2e7b7798da7a9353
Oleksandr Grytsov <oleksandr_grytsov@epam.com>
2020-10-21 18:07:53 +0300

    [crtmodules] Use proper package name
    Signed-off-by: Oleksandr Grytsov <oleksandr_grytsov@epam.com>

--- certmodules/modules.go
+++ certmodules/modules.go
@@ -15,7 +15,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package plugins
+package certmodules
 
 import (
 	// include all supported plugins
